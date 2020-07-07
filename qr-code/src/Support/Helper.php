c9f693ced143885cfa18fcb6d3105522598de34d
dela <xumeng1083@163.com>
2020-07-07 10:19:09 +0800

    v1.0

--- qr-code/src/Support/Helper.php
+++ qr-code/src/Support/Helper.php
@@ -8,33 +8,10 @@ trait Helper
     /**
      * 十六进制转换成RGB颜色值
      *
-     * @param $hex
+     * @param $color
      * @return array
      * @throws \Exception
      */
-    public function hexChangeRgb($hex)
-    {
-        if (!is_string($hex) && '#' !== $hex[0]) {
-            throw new \Exception('invalid hexadecimal color');
-        }
-
-        $hex = substr($hex, 1);
-
-        $color = [];
-        if (3 === strlen($hex)) {
-            $color['r'] = hexdec(substr($hex, 0, 1).substr($hex, 0, 1));
-            $color['g'] = hexdec(substr($hex, 1, 1).substr($hex, 1, 1));
-            $color['b'] = hexdec(substr($hex, 2, 1).substr($hex, 2, 1));
-        } elseif (6 === strlen($hex)) {
-            $color['r'] = hexdec(substr($hex, 0, 2));
-            $color['g'] = hexdec(substr($hex, 2, 2));
-            $color['b'] = hexdec(substr($hex, 4, 2));
-        } else {
-            throw new \Exception('invalid hexadecimal color count');
-        }
-
-        return $color;
-    }
 
     // #ff00ff -> array(255,0,255) or #f0f -> array(255,0,255)
     public static function hex2rgb($color)
